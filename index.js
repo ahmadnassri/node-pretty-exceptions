28445de941a65a48158952a8685eb20242f34f1d
Ahmad Nassri <ahmad@ahmadnassri.com>
2017-04-01 14:39:18 -0400

    feat(source): can now display source code!

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 const chalk = require('chalk')
 const ErrorStackParser = require('error-stack-parser')
+const fs = require('fs')
 const util = require('util')
 
+const showNative = process.env.PRETTY_EXCEPTIONS_NATIVE
+
 process.on('uncaughtException', function (error) {
+  // detect errors
   const isError = Object.prototype.toString.call(error) === '[object Error]' || error instanceof Error
 
+  // exit early
   if (!isError) {
     return console.error(error)
   }
@@ -24,23 +29,89 @@ process.on('uncaughtException', function (error) {
   console.error()
   console.error('%s%s %s', chalk.bold.white.bgRed(error.constructor.name), chalk.magenta(':'), error.message)
 
-  const files = Object.keys(grouped)
+  let filenames = Object.keys(grouped)
+
+  // detect native / readable files
+  filenames.forEach(filename => {
+    let stats = {
+      accessable: false,
+      native: false
+    }
+
+    try {
+      stats.accessable = fs.accessSync(filename, fs.constants.F_OK | fs.constants.R_OK)
+    } catch (err) {
+      // file inaccessable, likely native
+      stats.native = true
+    }
+
+    Object.assign(grouped[filename], stats)
+  })
+
+  if (!showNative) {
+    filenames = filenames.filter(filename => !grouped[filename].native)
+  }
 
   // loop through files
-  files.forEach((file, x) => {
+  filenames.forEach((filename, x) => {
+    const frames = grouped[filename]
+
+    const lastFile = x + 1 >= filenames.length
+
+    const PREFIX = lastFile ? ' ' : ' │'
+
+    let lines = false
 
     console.error(chalk.gray(' │'))
-    const symbol = x < files.length - 1 ? ' ├' : ' └'
-    const connector = x === files.length - 1 ? ' ' : ' │'
 
-    console.error('%s %s', chalk.gray(symbol + '─'), chalk.green(file))
-    console.error('%s   %s', chalk.gray(connector), chalk.gray('│'))
+    if (frames.accessable !== false) {
+      // read file content
+      lines = fs.readFileSync(filename).toString().split(/\n|\r\n/)
+    }
 
-    const frames = grouped[file]
+    // call stack frame
+    console.error(' %s %s', chalk.gray(lastFile ? '└┬╼' : '├─┬╼'), chalk.green(filename))
+
+    // spacer
+    console.error(chalk.gray(PREFIX + ' │'))
+
+    // loop through each frame
     frames.forEach((frame, n) => {
-      const symbol = n < frames.length - 1 ? '├' : '└'
+      const lastFrame = n + 1 >= frames.length
 
-      console.error('%s   %s %s %s %s %s %s', chalk.gray(connector), chalk.gray(symbol + '─'), chalk.gray('line:'), chalk.blue(frame.lineNumber), chalk.gray('col:'), chalk.cyan(frame.columnNumber), chalk.yellow(frame.functionName))
+      const symbol = frames.native && !lastFrame ? '├' : frames.native ? '└' : '├──╼'
+
+      // frame details
+      console.error('%s %s %s', chalk.gray(`${PREFIX} ${symbol}`), chalk.yellow(frame.functionName), chalk.gray.italic('@ line:'), chalk.blue.italic(frame.lineNumber))
+
+      if (lines) {
+        const line = lines[frame.lineNumber - 1]
+        const before = lines[frame.lineNumber - 2]
+        const after = lines[frame.lineNumber]
+
+        let whiteSpaceBefore = line.search(/\S|$/)
+
+        let preview = [before, line, after]
+
+        // spacer
+        console.error(chalk.gray(PREFIX + ' │'))
+
+        preview.forEach((item, y) => {
+          if (item.trim() !== '') {
+
+            if (item === line) {
+              console.error(chalk.gray(util.format(`${PREFIX} ${lastFrame ? '└' : '├'}╌╌╌%s╮`, '╌'.repeat(frame.columnNumber - whiteSpaceBefore))))
+            }
+
+            console.error(chalk.gray(`${PREFIX} ${lastFrame ? ' ' : '│'}    ${item.trim()}`))
+          }
+
+          if (y + 1 >= preview.length) {
+            console.error(chalk.gray(`${PREFIX} ${lastFrame ? '' : '│'}`))
+          }
+        })
+
+      }
     })
   })
 
